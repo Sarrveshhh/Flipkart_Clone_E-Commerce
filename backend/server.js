4b49886a3e000b276a4f7646a44b01d8e3fff3b0
Sarrveshhh <sarrvesharyaa@gmail.com>
2023-03-20 11:06:47 +0530

    First Commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+
+dotenv.config({path:"backend/config/config.env"});
+
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`Server working on https://localhost:${process.env.PORT}`)
+})
